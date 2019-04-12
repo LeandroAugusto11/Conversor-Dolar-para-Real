b119c8872f2671af3df29619cd3b76632a39177f
LeandroAugusto11 <leaugustooficial@gmail.com>
2019-04-12 13:21:32 -0300

    Create dolar.js

---
+++ dolar.js
@@ -0,0 +1,14 @@
+/**
+ * Conversor Dolar para Real
+ * @author Leandro Augusto e Matheus Motta
+ */
+function calcularCirculo() {
+    let dolar 
+
+    dolar = parseFloat(frmConversor.txtDolar.value.replace(",","."))
+
+    
+
+    frmConversor.txt.value = .toFixed(2)
+
+}
